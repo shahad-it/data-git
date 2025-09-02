6e7bbfae31e3268fdcf1391138b31d6d84082ebf
Shahad It <shahadalrehily@gmail.com>
2025-09-02 05:12:44 +0000

    Implement my_name_is function

--- today.py
+++ today.py
@@ -4,3 +4,9 @@ This is the module docstring
 
 
 pass  # YOUR CODE HERE
+"""A module computing buddy pair names for the day"""
+
+def my_name_is():
+    """Return my GitHub nickname"""
+    return "shahad-it"
+
