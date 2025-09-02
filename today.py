855007af5dc2049a91880c6f6b55e95a453521a5
Shahad It <shahadalrehily@gmail.com>
2025-09-02 05:37:56 +0000

    Implement my_name_is function

--- today.py
+++ today.py
@@ -6,6 +6,4 @@ This is the module docstring
 def my_name_is():
     """Return my GitHub nickname"""
     return "shahad-it"
-def my_buddy_is():
-    """Return my buddy's GitHub nickname"""
-    return "Maryam Al-Harthi"
+
